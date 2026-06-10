278b71570dca6566b0599b36e6b6eff626f26433
mushfika <musfika962@gmail.com>
2026-06-10 20:06:05 +0600

    take

---
+++ asycn.js
@@ -0,0 +1,21 @@
+console.log(1);
+console.log(2);
+console.log(3);
+doSomething();
+console.log(5);
+console.log(6);
+console.log(7);
+
+function doSomething() {
+  console.log(4);
+}
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// setTimeout(() => {
+//   console.log("lazy logged");
+// }, 2000);
+// console.log(5);
+// console.log(6);
+// console.log(7);
---
+++ contex.js
@@ -0,0 +1,9 @@
+let c = 3;
+
+function add(a, b) {
+  const result = a + b + c;
+  return result;
+}
+
+const sum = add(2, 4);
+console.log(sum);
