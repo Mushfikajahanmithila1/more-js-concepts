a3d5e2fbd91b2e10d042e2d0ce0cff8a0046ff41
mushfika <musfika962@gmail.com>
2026-06-10 20:45:41 +0600

    added

---
+++ interval.js
@@ -0,0 +1,18 @@
+console.log(1);
+console.log(2);
+console.log(3);
+// setTimeout(() => {
+//   console.log("lazy logged");
+// }, 2000);
+console.log(5);
+console.log(6);
+console.log(7);
+
+setInterval(() => {
+  const num = 0;
+  if (num > 20) {
+    clearInterval();
+  }
+  num += 1;
+  console.log(num);
+}, 2000);
